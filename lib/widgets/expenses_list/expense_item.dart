49ef3c4cdf3c547d27e74718d04ddfd98c8b054f
Dhruv Nayaran Jaiswal <dhruv@Dhruvs-MacBook-Air.local>
2024-04-25 13:23:41 +0530

    first commit initialised

---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 20,
+          ),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text(expense.title, 
+                  style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 16, color:Color.fromARGB(255, 68, 68, 68))
+                  
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              Row(
+                children: [
+                  Text('\$${expense.amount.toStringAsFixed(2)}'),
+                  const Spacer(),
+                  Row(
+                    children: [
+                    Icon(categoryIcons[expense.category]),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(expense.formattedDate),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+          ),
+    );
+  }
+}
